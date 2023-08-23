6e5ba27f8ce1031faa208c6577783678b82de015
Johans Neira <johansneirap@gmail.com>
2023-08-22 23:56:19 -0400

    feat: google login integrattion with frontend

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
-
+import axios from 'axios';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -66,5 +66,35 @@ export class AuthController {
       secure: true,
     });
     res.redirect('http://localhost:3000');
+  }
+
+  @Post('oauth/google')
+  async googleOauth(@Req() req: Request, @Res() res: Response) {
+    const { user } = req.body;
+    const url = `https://oauth2.googleapis.com/tokeninfo?access_token=${user.accessToken}`;
+    const { data } = await axios.get(url);
+    console.log(user, 'USER');
+    console.log(data, 'RESPONSE FROM GOOGLE');
+    //TODO: Register OR update the user in the database
+    if (data.sub !== user.id)
+      return res.status(401).json({ message: 'Unauthorized' });
+
+    const token = await this.authService.signToken(user.id, user.email);
+    res.json(token);
+  }
+
+  @Post('oauth/facebook')
+  async facebookOauth(@Req() req: Request, @Res() res: Response) {
+    const { user } = req.body;
+    const url = `https://graph.facebook.com/me?access_token=${user.accessToken}`;
+    const { data } = await axios.get(url);
+    console.log(user, 'USER');
+    console.log(data, 'RESPONSE FROM FACEBOOK');
+    //TODO: Register OR update the user in the database
+    if (data.id !== user.id)
+      return res.status(401).json({ message: 'Unauthorized' });
+
+    const token = await this.authService.signToken(user.id, user.email);
+    res.json(token);
   }
 }
